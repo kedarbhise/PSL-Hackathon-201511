c05cce53c3da5c1721e7cd60f097301d647f2aa9
shipra <shipra_singhal@persistent.co.in>
2015-11-27 17:36:27 +0530

    updated nodejs

--- nodejs/start.js
+++ nodejs/start.js
@@ -3,65 +3,158 @@ var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
 var app = express();
-var content={{"name":"abc","email":"abc@gmail.com"},{"name":"xyz","email":"xyz@gmail.com"}};
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'Gaig!@#123',
+  database : 'hackathon'
+});
 
+
+var content=[{"name":"abc","email":"abc@gmail.com","url":""},{"name":"xyz","email":"xyz@gmail.com","url":""}];
+var checkCustomerdata=false;
 //request=request.defaults({'proxy':'http://shipra_singhal:saurabh@119@hjproxy.persistent.co.in:8080'});
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-app.get('/getDetails', function(req, res) {
-	
-});
-app.post('/postDetails', function(req, res) {
-	console.log("Body: "+JSON.stringify(req.body));	
-	
-	console.log("Content: "+JSON.stringify(content));
-	postMarks(req, res);
-});
-
-
-//Post Marks
-function postMarks(req, res){
-request({	url: 'https://gateway.watsonplatform.net/tradeoff-analytics/api/v1/dilemmas',
-			method: 'POST',
-			body: JSON.stringify(content),
-			headers: {
-				'Authorization':'Basic ZTNiNjA2ZmUtMDM4My00MzlhLTg2N2MtNGMyMGJmM2Y1OThhOlVSbWxQaWlJMTVoSQ==',
-				'Content-Type':'application/json'
-				}
-			
-		},
-			function(error, response, body){
-				console.log("Fetching Data");
-				//console.log(error+": "+response.statusCode);
-				if (!error && response.statusCode == 200) {
-					console.log("successful POST");
-					//res.send(JSON.parse(body));
-					var result=JSON.parse(body).resolution; 
-					var frontResult="<b>Top-Options</b> : ";
-					var excludedResult="<b>Excluded-Options</b> : "; 
-					for(var i=0;i<3;i++){
-						if(result.solutions[i].status == "FRONT"){
-							if((result.solutions[i].solution_ref)==(content.options[i].key))
-							{
-							frontResult+=content.options[i].name+" ";
-							}
-						}
-						else{
-							if((result.solutions[i].solution_ref)==(content.options[i].key))
-							{
-							excludedResult+=content.options[i].name+" ";
-							}
-						}														
-					}
-					res.send(frontResult+"<br><br>"+excludedResult);	
-				}
-				else{
-				res.send("Error:"+error);
-				}			
-			}	
-		);
+//to make db connection
+connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... \n\n");  
+} else {
+    console.log("Error connecting database ... \n\n");  
 }
+});
+
+//To get requested user's details
+app.get('/getRequestQueue', function(req, res) {
+	connection.query('SELECT * FROM Requests a, Customer b where a.EmailId=b.EmailId', function(err, rows, fields) {
+	//connection.end();
+  if (!err){
+    console.log('The solution is: ', rows);
+	res.send(rows);
+	}
+  else{
+    console.log('Error while performing Query.');
+  }
+  });
+
+		
+});
+
+//To get requested user's details
+app.get('/getEnergyUsage', function(req, res) {
+	var customerId = req.query.CustomerId;
+	console.log("customerId: "+customerId);
+	connection.query('SELECT * FROM EnergyUsage where CustomerId='+customerId, function(err, rows, fields) {
+	
+  if (!err){
+    console.log('The solution is: ', rows);
+	res.send(rows);
+	}
+  else{
+    console.log('Error while performing Query.');
+  }
+  });
+
+		
+});
+
+
+//To post user's details
+app.post('/postHelpRequest', function(req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	var body=req.body;
+	console.log(body); 
+
+	var query1="INSERT INTO Requests (EmailId, isServiced) VALUES ('"+body.EmailId+"',0)";
+	connection.query(query1,function(err, rows, fields) {
+	if (err) {
+		console.error(err);
+		res.statusCode = 500;
+		res.send(JSON.stringify({
+			result: 'error',
+			err: err.code
+		}));
+	}
+	else{	
+		console.log("Record saved to Requests table");
+		/*var check=getCustomerDetails(body.EmailId);
+		console.log("check:"+check);*/
+		res.send(JSON.stringify({
+				result: 'success',
+				json: rows,
+				length: rows.length
+				}));
+		//connection.end();
+	}	 
+	});
+
+});
+
+
+//To update URL
+app.post('/postMeetURL', function(req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	var body=req.body;
+	console.log(body); 
+	var query="UPDATE Requests SET MeetingURL='"+body.URL+"', isServiced=1 WHERE RequestId="+body.RequestId;
+	connection.query(query,function(err, rows, fields) {
+	if (err) {
+		console.error(err);
+		res.statusCode = 500;
+		res.send(JSON.stringify({
+			result: 'error',
+			err: err.code
+		}));
+	}
+	else{	
+		console.log("Record updated to db");
+		res.send(JSON.stringify({
+		result: 'success',
+		json: rows,
+		length: rows.length
+		}));
+		//connection.end();
+	}	 
+	});
+
+});
+
+app.get('/getMeetURL', function(req, res) {
+	var requestId = req.query.RequestId;
+	console.log("requestId: "+requestId);
+	connection.query('SELECT MeetingURL FROM Requests where RequestId='+requestId, function(err, rows, fields) {
+	
+  if (!err){
+    console.log('The solution is: ', rows);
+	res.send(rows);
+	}
+  else{
+    console.log('Error while performing Query.');
+  }
+  });
+
+		
+});
+
+/*function getCustomerDetails(emailId){
+	connection.query("SELECT * FROM Customer where EmailId='"+emailId+"'", function(err, rows, fields) {
+	//connection.end();
+  if (!err){
+    console.log('The solution is: ', rows);
+    return true;
+	//res.send(rows);
+	}
+  else{
+    console.log('Error while performing Search on Customer.');
+  }
+  });
+}*/
 
 app.listen(3000);
 
